ff6da8ee174a8664be9e18e82c9a607ec2480948
Chen Pang <1482561024@qq.com>
2025-07-10 18:45:36 +0800

    before starting pa1

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
+#include "verilated.h"
+#include <nvboard.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+void nvboard_bind_all_pins(Vtop* top);
+Vtop* top = new Vtop;
+
+void single_cycle() {
+  top->clk = 0; top->eval();
+  top->clk = 1; top->eval();
+}
+
+void reset(int n) {
+  top->rst = 1;
+  while (n -- > 0) single_cycle();
+  top->rst = 0;
+}
+
+int main(int argc, char** argv)
+{
+    
+    Verilated::commandArgs(argc, argv);
+    nvboard_bind_all_pins(top);
+    nvboard_init();
+    reset(10);  
+    while(1) 
+    {
+        nvboard_update();
+        if(top->clk == 1||top->rst == 1)
+          single_cycle();
+        nvboard_update();
+    }
 }
